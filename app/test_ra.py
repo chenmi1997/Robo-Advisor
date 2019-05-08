d5006f4db846e63228cb341671ec0461a3b32126
chenmi1997 <46470453+chenmi1997@users.noreply.github.com>
2019-05-08 14:21:42 -0400

    Update test_ra.py

--- app/test_ra.py
+++ app/test_ra.py
@@ -1,33 +1,11 @@
-from robo_advisor import to_usd, get_response
-from dotenv import load_dotenv
-import json
-import os
-import requests
-import pandas as pnd 
-from datetime import datetime
+# ROBO ADVISOR TEST
 
-#testing dollar value conversion
-def test_to_usd():
+from robo_advisor import to_usd
+
+def test_usd():
     result = to_usd(10)
-    result1 = to_usd(1000)
     result2 = to_usd(57.3333)
     result3 = to_usd(.011111111)
     assert result == "$ 10.00"
-    assert result1 == "$ 1000.00"
     assert result2 == "$ 57.33"
     assert result3 == "$ 0.01"
-
-#testing responses from API Key and csv reading
-def test_getresponse():
-    ticker = "AAPL"
-    apikey = str(os.environ.get("ALPHAVANTAGE_API_KEY"))
-    
-    answer = get_response("AAPL", apikey)
-
-    parsed_answer = json.loads(answer.text)
-    
-    
-    
-    result = list(parsed_answer)
-
-    assert result == ['Meta Data', 'Time Series (Daily)']
